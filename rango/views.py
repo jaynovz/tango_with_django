044aa87059f3c0432fa0d164245a57b63c9c954c
leia <jpmitchem@gmail.com>
2015-03-12 23:24:03 -0400

    went back and did extra exercises in chap 5 and 6

--- rango/views.py
+++ rango/views.py
@@ -13,10 +13,15 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
 
     for category in category_list:
         category.url = category.name.replace(' ', '_')
+
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    for page in page_list:
+        page.url = page.title.replace(' ', '_')
 
     # Render the response and send it back!
     return render_to_response('rango/index.html', context_dict, context)
