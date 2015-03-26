c653f0df62a063c7c07fa56b6dc65885981450e3
leia <jpmitchem@gmail.com>
2015-03-25 21:36:00 -0400

    the path to hell is paved with hard coded paths -- revelation 20:16

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -114,7 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/leia/Code/tango_with_django_project/tango_with_django_project/templates'
+    TEMPLATE_PATH,
 )
 INSTALLED_APPS = (
     'django.contrib.auth',
